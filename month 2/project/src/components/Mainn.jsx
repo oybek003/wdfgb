4c695704965b32306bb28f8d24b75341e5cb5737
oybek003 <@ojbekmuslimov1.com>
2025-07-12 13:40:06 +0500

    liuhgv

--- month 2/project/src/components/Mainn.jsx
+++ month 2/project/src/components/Mainn.jsx
@@ -3,7 +3,17 @@ import React from 'react'
 const main = () => {
   return (
     <div>
-        <h1>wkjedhgv</h1>
+      <nav className='flex items-center justify-between mr-[32px] ml-[32px]'>
+          <h1>Portfolio</h1>
+
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Portfolio</li>
+            <li>Contact</li>
+          </ul>
+      </nav>
     </div>
   )
 }
