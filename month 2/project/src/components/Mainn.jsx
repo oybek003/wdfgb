bafc281f4e75ef8fd5edbc265c1834129be15c32
oybek003 <@ojbekmuslimov1.com>
2025-07-12 13:46:26 +0500

    ojhv

--- month 2/project/src/components/Mainn.jsx
+++ month 2/project/src/components/Mainn.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 const main = () => {
   return (
     <div>
-      <nav className='flex items-center justify-between mr-[32px] ml-[32px]'>
+      <nav className='flex items-center justify-between mr-[32px] ml-[32px] mt-[32px]'>
           <h1>Portfolio</h1>
 
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Services</li>
-            <li>Portfolio</li>
-            <li>Contact</li>
+          <ul className='flex items-center gap-[32px]'>
+            <li className='hover:text-blue-400'>Home</li>
+            <li className='hover:text-blue-400'>About</li>
+            <li className='hover:text-blue-400'>Services</li>
+            <li className='hover:text-blue-400'>Portfolio</li>
+            <li className='hover:text-blue-400'>Contact</li>
           </ul>
       </nav>
     </div>
